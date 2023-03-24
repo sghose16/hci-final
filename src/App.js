4fc199bc98b9251915c4a268396215cfca6f99ea
lillytian <lillytian2001@gmail.com>
2023-03-24 09:54:23 -0500

    testing item blocks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import DisplayItemsContainer from "./components/DisplayItemsContainer";
+import ChooseItemsContainer from "./components/ChooseItemsContainer";
+
+// assets
+import top1 from "./assets/top1.png";
+import top2 from "./assets/top2.png";
 
 function App() {
   return (
@@ -18,6 +24,9 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* code for testing the item blocks */}
+      {/* <DisplayItemsContainer title={"Tops"} items={[top1, top2]} />
+      <ChooseItemsContainer title={"Tops"} selected={[top1, top2]} /> */}
     </div>
   );
 }
