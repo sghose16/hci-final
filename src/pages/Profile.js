f7af8ee05c86c6468be10e483826aa91fe1dea72
swetagh <swetaghose16@gmail.com>
2023-03-24 17:45:11 -0500

    profile page settings/photo

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,70 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import settings from '../assets/settings.png'
+import john from '../assets/john.png';
+
+import { Box } from '@mui/material';
+
+
 
 function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-      <div>
-        <Link to="/closet">Closet</Link>
-      </div>
-      <div>
-        <Link to="/outfit">Outfit</Link>
-      </div>
+    return (
+        // <div>
+        //     <div class="logo" style= {{float: "right",   height: '50px'}}> 
+        //         <img src={settings}/> 
+        //     </div>
+        
+        //     <div className='center'>
+        //       <img src={cur} alt="react logo" style={{ width: '400px', }}/>
+        //     </div>
 
-    </div>
-  );
-}
 
-export default Profile;
+        // </div>
+        <>
+            {/* // <div>
+            //     <div class="logo" style= {{float: "right",   height: '50px'}}> 
+            //         <img src={settings}/> 
+            //     </div>
+            //     <div className='center'>
+            //       <img src={cur} alt="react logo" style={{ width: '400px', }}/>
+            //     </div>
+            // </div> */}
+            <Box sx={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                p: 1,
+                m: 1,
+                bgcolor: 'background.paper',
+                borderRadius: 1,
+            }}>
+                <img src={settings} />
+            </Box>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    p: 1,
+                    m: 1,
+                    bgcolor: 'background.paper',
+                    borderRadius: 1,
+                }}
+            >  <img src={john} /> 
+                </Box>
+                
+                <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    p: 1,
+                    m: 1,
+                    bgcolor: 'background.paper',
+                    borderRadius: 1,
+                }}
+            > John
+                </Box>
+                
+        
+        </>
+
+    );
+  }
+
+  export default Profile;
