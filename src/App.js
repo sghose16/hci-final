1345f49c5ecab68bdc809905c2e67d1d191e9e29
John Powers <johnpowers3737@utexas.edu>
2023-03-29 21:37:56 -0500

    adding creating outfit thingy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import Settings from "./pages/Settings";
 import React from "react";
 import Profile from "./pages/Profile";
 import ShowAll from "./pages/ShowAll";
+import ShowAllSelectable from "./pages/ShowAllSelectable";
 import Navbar from "./components/Navbar";
+import CreateOutfit from "./pages/CreateOutfit";
 
 function App() {
   const BrowserRouter = createBrowserRouter(
@@ -20,6 +22,7 @@ function App() {
       <Route path="/outfit" element={<Outfit />} />,
       <Route path="/profile" element={<Profile />} />,
       <Route path="/settings" element={<Settings />} />,
+      <Route path="/create-outfit" element={<CreateOutfit />} />,
 
       <Route path="/all-tops" element={<ShowAll type={"tops"} />} />,
       <Route path="/all-bottoms" element={<ShowAll type={"bottoms"} />} />,
@@ -27,6 +30,13 @@ function App() {
       <Route
         path="/all-accessories"
         element={<ShowAll type={"accessories"} />}
+      />,
+      <Route path="/all-selectable-tops" element={<ShowAllSelectable type={"tops"} />} />,
+      <Route path="/all-selectable-bottoms" element={<ShowAllSelectable type={"bottoms"} />} />,
+      <Route path="/all-selectable-footwear" element={<ShowAllSelectable type={"footwear"} />} />,
+      <Route
+        path="/all-selectable-accessories"
+        element={<ShowAllSelectable type={"accessories"} />}
       />,
 
       <Route path="*" element={<Closet />} />,
