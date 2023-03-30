98d6386808ec03e6f78fe22614e0f4ba24b5a0a4
John Powers <johnpowers3737@utexas.edu>
2023-03-29 21:57:11 -0500

    added back button to create outfits page

--- src/pages/CreateOutfit.js
+++ src/pages/CreateOutfit.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { Container, Grid } from "@mui/material";
+import { ArrowBackIosNew } from "@mui/icons-material";
+import { Button, Container, Grid } from "@mui/material";
 import ChooseItemsContainer from "../components/ChooseItemsContainer";
+import { Link } from "react-router-dom";
 
 function CreateOutfit() {
     const [top, setTop] = useState([]);
@@ -8,46 +10,56 @@ function CreateOutfit() {
     const [foot, setFoot] = useState([]);
     const [accessory, setAccessory] = useState([]);
 
-  return (
-    <Container>
-      {/* title of page */}
-      <Grid container>
-        <Grid item>
-          <h1>Create Outfit</h1>
-        </Grid>
-      </Grid>
+    return (
+        <Container>
 
-      <Grid container rowSpacing={2}>
-        <Grid item xs={12}>
-          <ChooseItemsContainer
-            title={"Tops"}
-            selected={top}
-          />
-        </Grid>
+            <Grid container>
+                <Grid item>
+                    <Link to="/outfit">
+                        <Button startIcon={<ArrowBackIosNew />}>Back</Button>
+                    </Link>
+                </Grid>
+            </Grid>
 
-        <Grid item xs={12}>
-          <ChooseItemsContainer
-            title={"Bottoms"}
-            selected={bottom}
-          />
-        </Grid>
 
-        <Grid item xs={12}>
-          <ChooseItemsContainer
-            title={"Footwear"}
-            selected={foot}
-          />
-        </Grid>
+            {/* title of page */}
+            <Grid container>
+                <Grid item>
+                    <h1>Create Outfit</h1>
+                </Grid>
+            </Grid>
 
-        <Grid item xs={12}>
-          <ChooseItemsContainer
-            title={"Accessories"}
-            selected={accessory}
-          />
-        </Grid>
-      </Grid>
-    </Container>
-  );
+            <Grid container rowSpacing={2}>
+                <Grid item xs={12}>
+                    <ChooseItemsContainer
+                        title={"Tops"}
+                        selected={top}
+                    />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <ChooseItemsContainer
+                        title={"Bottoms"}
+                        selected={bottom}
+                    />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <ChooseItemsContainer
+                        title={"Footwear"}
+                        selected={foot}
+                    />
+                </Grid>
+
+                <Grid item xs={12}>
+                    <ChooseItemsContainer
+                        title={"Accessories"}
+                        selected={accessory}
+                    />
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default CreateOutfit;
