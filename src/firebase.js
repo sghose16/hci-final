760b8fe16e7f8c5d5ce23ebd44c1990045bb6768
swetagh <swetaghose16@gmail.com>
2023-04-06 15:39:56 -0500

    new firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,21 +5,17 @@ import { getDatabase} from "firebase/database";
 import { getStorage} from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB4y_Ku1jlpEEme00JgLBQ_z8OlJ7_sPrU",
-  authDomain: "pocket-71bd9.firebaseapp.com",
-  databaseURL: "https://pocket-71bd9-default-rtdb.firebaseio.com",
-  projectId: "pocket-71bd9",
-  storageBucket: "pocket-71bd9.appspot.com",
-  messagingSenderId: "507295040718",
-  appId: "1:507295040718:web:65e5d498db6d4e869a3c9f",
-  measurementId: "G-JGXSB0QF3R"
+  apiKey: "AIzaSyArAoa0otKk1AvP6UFi-nIOrhvVz20p_oI",
+  authDomain: "pocket-pt2.firebaseapp.com",
+  projectId: "pocket-pt2",
+  storageBucket: "pocket-pt2.appspot.com",
+  messagingSenderId: "1069225398984",
+  appId: "1:1069225398984:web:33f88f20fd34b11f1e0bfd"
 };
 
 // Initialize Firebase
