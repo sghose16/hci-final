b45e8badd55f5894d0132a21c74d62ade66fc200
swetagh <swetaghose16@gmail.com>
2023-04-07 18:28:54 -0500

    no longer need backend calls to filter/favorite out

--- src/pages/closet/ShowAll.js
+++ src/pages/closet/ShowAll.js
@@ -1,5 +1,5 @@
 import { ArrowBackIosNew } from "@mui/icons-material";
-import { Button, Container, Grid, IconButton } from "@mui/material";
+import { Button, Container, Grid, IconButton, Box, TextField } from "@mui/material";
 import { Select, MenuItem } from "@material-ui/core";
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
@@ -14,68 +14,74 @@ import ViewItemDialogContainer from "../../components/ViewItemDialogContainer";
 
 function ShowAll(props) {
   const [items, setItems] = useState([]);
+
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [index, setIndex] = useState(0);
+  
   const [favoriteItems, showFavorites] = useState(false);
+
+  const [brand, showBrand] = useState("");
+  const [filter, setFilter] = useState(false);
+
+  const [all, setAll] = useState([]);
 
   const showFavoriteItems = () => {
     showFavorites(!favoriteItems);
   }
 
+  const handleBrandChange = (event) => {
+    showBrand(event.target.value);
+  };
+
+  const handleFilter =  () => {
+    setFilter(!filter);
+  };
+
+  function filterItems(items, filter, filterValue) {
+    const filteredItems = [];
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      let itemVal = item[filter];
+      if (filter === "brand"){
+        itemVal = itemVal.toLowerCase().trim();
+      }
+      if (itemVal === filterValue) {
+        filteredItems.push(item);
+      }
+    }
+    return filteredItems;
+  }
+
+
   const getItems = () => {
     const auth = getAuth();
     const userId = auth.currentUser.uid;
-    const dbRef = ref(getDatabase());
-
-      console.log(favoriteItems);
-      if (favoriteItems){
-        
-        const itemsRef = ref(database, `users/${userId}/items/${props.type}`);
-        const favoriteItemsQuery = query(itemsRef, orderByChild("favorite"), equalTo(true));
-        get(favoriteItemsQuery).then((snapshot) => {
-          if (snapshot.exists()) {
-            const favoriteItems = Object.values(snapshot.val());
-            setItems(favoriteItems);
-          }
-        }).catch((error) => {
-          console.log(error);
-        });
-
-      }else{
-        get(child(dbRef, `users/${userId}/items/${props.type}`))
-        .then((snapshot) => {
-          if (snapshot.exists()) {
-            setItems(Object.values(snapshot.val()));
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });  
+    const itemsRef = ref(database, `users/${userId}/items/${props.type}`);
+    get(itemsRef).then((snapshot) => {
+      console.log("call to backend in get");
+      if (snapshot.exists()) {
+        let listItems = Object.values(snapshot.val());
+        setItems(listItems);
+        setAll(listItems);
       }
+    })
+    .catch((error) => {
+      console.log(error);
+    });  
+   
+    // old way of getting favorite items from backend don't want to delete yet 
+    //   if (favoriteItems){
+    //   const favoriteItemsQuery = query(itemsRef, orderByChild("favorite"), equalTo(true));
+    //   get(favoriteItemsQuery).then((snapshot) => {
+    //     if (snapshot.exists()) {
+    //       const favoriteItems = Object.values(snapshot.val());
+    //       setItems(favoriteItems);
+    //     }
+    //   }).catch((error) => {
+    //     console.log(error);
+    //   });
+
   };
-
-  const renderItems = () => {
-    if (items.length === 0) {
-      return <div>No {props.type} found.</div>;
-    }
-
-    return items.map((item, index) => {
-      return (
-        <Grid item xs={4} key={item["id"]}>
-          <div
-            className="img-container"
-            onClick={() => {
-              setIndex(index);
-              setIsDialogOpen(true);
-            }}
-          >
-            <img src={item["img"]} className="img-square" />
-          </div>
-        </Grid>
-      );
-    });
-  };
-
 
   const handleDelete = (item) => {
     const auth = getAuth();
@@ -85,6 +91,8 @@ function ShowAll(props) {
     // get ref to item with item.id
     get(child(dbRef, `users/${userId}/items/${props.type}`)).then(
       (snapshot) => {
+        console.log("call to backend in delete");
+
         if (snapshot.exists()) {
           const allItems = snapshot.val();
           // find the index of the item to delete
@@ -112,6 +120,8 @@ function ShowAll(props) {
 
     // delete the item from the state
     setItems(items.filter((i) => i.id !== item.id));
+    setAll(all.filter((i) => i.id !== item.id));
+
     setIsDialogOpen(false);
   };
 
@@ -123,6 +133,8 @@ function ShowAll(props) {
     // get ref to item with item.id
     get(child(dbRef, `users/${userId}/items/${props.type}`)).then(
       (snapshot) => {
+        console.log("call to backend in save");
+
         if (snapshot.exists()) {
           const allItems = snapshot.val();
           // find the index of the item to update
@@ -149,9 +161,17 @@ function ShowAll(props) {
       }
     );
 
-    // update the item in the state
     setItems(
       items.map((i) => {
+        if (i.id === item.id) {
+          return item;
+        }
+        return i;
+      })
+    );
+
+    setAll(
+      all.map((i) => {
         if (i.id === item.id) {
           return item;
         }
@@ -161,9 +181,53 @@ function ShowAll(props) {
     setIsDialogOpen(false);
   };
 
+  //No calls to backend just filter out displayed items from all items
+  useEffect(() => {
+    if (filter){
+      let listItems = filterItems(all, "brand", brand)
+      setItems(listItems);
+    }else{
+      setItems(all);
+    }
+  }, [filter]);
+
+  useEffect(() => {
+    if (favoriteItems){
+      let listItems = filterItems(all, "favorite", true)
+      setItems(listItems);
+    }else{
+      setItems(all);
+    }
+  }, [favoriteItems]);
+
+  // Doing the backend call the first  time and getting all items 
   useEffect(() => {
     getItems();
-  }, [favoriteItems]);
+  }, []);
+
+
+  const renderItems = () => {
+    if (items.length === 0) {
+      return <div>No {props.type} found.</div>;
+    }
+
+    return items.map((item, index) => {
+      return (
+        <Grid item xs={4} key={item["id"]}>
+          <div
+            className="img-container"
+            onClick={() => {
+              setIndex(index);
+              setIsDialogOpen(true);
+            }}
+          >
+            <img src={item["img"]} className="img-square" />
+          </div>
+        </Grid>
+      );
+    });
+  };
+
 
   return (
     <Container>
@@ -191,14 +255,24 @@ function ShowAll(props) {
 
       </Grid>
 
-      {/* <Select value={selectedAttribute} onChange={handleChange}>
-          Filter By
-        {attributeOptions.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
-          </MenuItem>
-        ))}
-      </Select> */}
+              {/* filter by brand
+          <form>
+          <Box display="flex" flexDirection="row" mb={2}>
+            <Box mr={2}>
+              <TextField
+                fullWidth
+                placeholder="Brand"
+                value={brand}
+                onChange={handleBrandChange}
+              />
+            </Box>
+            <Button variant="contained"  disabled={brand.trim().length === 0} onClick={handleFilter}>
+          Filter
+             </Button>
+          
+          </Box>
+        </form> */}
+
 
       {/* all items under category */}
       <Grid container spacing={2}>
